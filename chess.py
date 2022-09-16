7c654af4ffa004149608351e4315fe763398f304
blu-pi <brianbusch2002@hotmail.com>
2022-09-16 16:52:31 +0100

    Added Tier 1 check for knight moves. Added extra features to Position and Square objects

--- chess.py
+++ chess.py
@@ -20,11 +20,24 @@ class Pos: #cba to find import for co-ordinates
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        if (isinstance(other, Pos)):
+            return self.x == other.x and self.y == other.y
+        return False
+
+    def __add__(self, other):
+        return Pos(self.x + other.x, self.y + other.y)  
+    #__radd__ not needed as only 2 Pos can be added. Pos + int or int + Pos is not supported!
+
+    #checks if the position is on the board
+    def isValid(self):
+        return self.x >= 0 and self.x < 8 and self.y >= 0 and self.y < 8
+
     def toSqu(self):
         return board_files[self.x, self.y + 1]
 
     def __str__(self):
-        return str(self.x)+","+str(self.y)
+        return "[ " + str(self.x) + "," + str(self.y) + " ]"
 
 
 #squ always refers to a square as people see it e.g. E4
@@ -34,7 +47,12 @@ class Squ:
     def __init__(self, file, rank):
         self.file = file
         self.rank = rank
+
+    #checks if the square exists on the board
+    def isValid(self):
+        return self.file in board_files and self.rank >= 0 and self.rank < 9
     
+    #convert Squ to Pos. Does NOT check for validity! Input must be valid
     def toPos(self):
         counter = -1
         for file in board_files:
@@ -77,10 +95,25 @@ class Knight(Piece):
     
     #return co-ordinates of all squares this peace is allowed to move to. 
     #Start Tier 1 legality check (refer to "Legality Logic Explained")
-    def get_piece_moves(self):
+    def get_possible_moves(self):
         possible_new_pos = [] 
-        #TODO carry on making these
-
+        delta_x = 2
+        delta_y = 1
+        for i in range(8):
+            if i == 4: #flip deltas to create other 4 moves
+                delta_x = 1
+                delta_y = 2
+            move_vector = Pos(delta_x, delta_y)
+            new_pos = self.pos + move_vector
+            if new_pos.isValid:
+                possible_new_pos.append(new_pos)
+            #modify deltas for next loop
+            if i % 2 == 0:
+                delta_y *= -1
+            else:
+                delta_x *= -1           
+        print(possible_new_pos + " DEBUG TEST")
+        #proceed to Tier 3 legality checks    
 
 
 class Bishop(Piece):
